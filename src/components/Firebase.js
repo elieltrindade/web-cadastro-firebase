158050daa74e1eedfe9f7c33e786e276294985e9
Eliel trindade <123204237+elieltrindade@users.noreply.github.com>
2024-04-20 20:21:19 -0300

    Update Firebase.js

--- src/components/Firebase.js
+++ src/components/Firebase.js
@@ -4,12 +4,7 @@ import 'firebase/auth'
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBroKH9CEk5eh7bQsFluZAJaC2h9TXymdg",
-    authDomain: "projetocadastroreact.firebaseapp.com",
-    projectId: "projetocadastroreact",
-    storageBucket: "projetocadastroreact.appspot.com",
-    messagingSenderId: "412717300603",
-    appId: "1:412717300603:web:59185a9a9addb9b694dbde"
+
   };
 
   if(!firebase.apps.length){
